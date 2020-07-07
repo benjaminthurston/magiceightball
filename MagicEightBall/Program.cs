fe4b112e8cacb8de9cb79b072411879c17c89374
benjaminthurston <benjamin.thurston87@gmail.com>
2020-07-07 18:58:26 -0400

    initial commit

---
+++ MagicEightBall/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MagicEightBall
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
