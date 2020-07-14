af2a01e37a8076784ab0a885695ee97c59c1901b
benjaminthurston <benjamin.thurston87@gmail.com>
2020-07-14 17:44:08 -0400

    adding answers

--- MagicEightBall/Program.cs
+++ MagicEightBall/Program.cs
@@ -1,12 +1,57 @@
 ﻿using System;
 
-namespace MagicEightBall
+namespace eightball
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string[] answers = 
+            {
+                "Yes",
+                "No",
+                "Ask the next guy!",
+                "Signs point to yes",
+                "Are you sure you want me to answer that?",
+                "Think on that question, take a break and ask it again",
+                "Go ask that other eight ball! I hear they are nicer.",
+                "Are you flirting with me?",
+                "I need a nap",
+                "It is certain.",
+                "It is decidedly so.",
+                "Without a doubt.",
+                "Yes – definitely.",
+                "You may rely on it.",
+                "As I see it, yes.",
+                "Most likely.",
+                "Outlook good.",
+                "Reply hazy, try again.",
+                "Not da mama",
+                "Better not tell you now.",
+                "Cannot predict now.",
+                "Concentrate and ask again.",
+                "Don't count on it.",
+                "My reply is no.",
+                "My sources say no.",
+                "Outlook not so good.",
+                "Very doubtful."
+
+
+
+            };
+            while (true)
+            {
+                Random rnd = new Random();
+                int result = rnd.Next(0, 26);
+
+                Console.WriteLine("The not so mighty Eight Ball awaits your question! Ask a question and hit the enter key for the answer!");
+
+                string inp = Console.ReadLine();
+
+                Console.WriteLine(answers[result]);
+
+
+            }
         }
     }
 }
