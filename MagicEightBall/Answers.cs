96948dc72d635eb886f1d33c36b5bb1db9c8ba1e
benjaminthurston <benjamin.thurston87@gmail.com>
2020-07-14 20:35:49 -0400

    Adding Deserialization

---
+++ MagicEightBall/Answers.cs
@@ -0,0 +1,15 @@
+using System.Text.Json.Serialization;
+
+namespace eightball
+{    
+    
+    public class Answers
+    {
+        [JsonPropertyName("id")]
+        public int Id {get; set;}
+
+        [JsonPropertyName("answer")]
+        public string Answer {get; set;}
+
+    }
+}
--- MagicEightBall/Program.cs
+++ MagicEightBall/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.IO;
 
 namespace eightball
 {
@@ -6,52 +10,22 @@ namespace eightball
     {
         static void Main(string[] args)
         {
-            string[] answers = 
-            {
-                "Yes",
-                "No",
-                "Ask the next guy!",
-                "Signs point to yes",
-                "Are you sure you want me to answer that?",
-                "Think on that question, take a break and ask it again",
-                "Go ask that other eight ball! I hear they are nicer.",
-                "Are you flirting with me?",
-                "I need a nap",
-                "It is certain.",
-                "It is decidedly so.",
-                "Without a doubt.",
-                "Yes – definitely.",
-                "You may rely on it.",
-                "As I see it, yes.",
-                "Most likely.",
-                "Outlook good.",
-                "Reply hazy, try again.",
-                "Not da mama",
-                "Better not tell you now.",
-                "Cannot predict now.",
-                "Concentrate and ask again.",
-                "Don't count on it.",
-                "My reply is no.",
-                "My sources say no.",
-                "Outlook not so good.",
-                "Very doubtful."
-
-
-
-            };
+            var fileData = File.ReadAllText("Answer.json");
+            var answers = JsonSerializer.Deserialize<List<Answers>>(fileData);
             while (true)
             {
-                Random rnd = new Random();
-                int result = rnd.Next(0, 26);
+                    Random rnd = new Random();
+                    int result = rnd.Next(0, answers.Count-1);
 
-                Console.WriteLine("The not so mighty Eight Ball awaits your question! Ask a question and hit the enter key for the answer!");
+                    Console.WriteLine("The not so mighty Eight Ball awaits your question! Ask a question and hit the enter key for the answer!");
 
-                string inp = Console.ReadLine();
+                    string inp = Console.ReadLine();
 
-                Console.WriteLine(answers[result]);
+                    Console.WriteLine(answers[result].Answer);
 
 
             }
+        
         }
     }
 }
