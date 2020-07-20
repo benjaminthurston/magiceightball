50793f0186101c88b669e2a3795476c944d7bee7
benjaminthurston <benjamin.thurston87@gmail.com>
2020-07-19 20:51:28 -0400

    adding comments

--- MagicEightBall/Program.cs
+++ MagicEightBall/Program.cs
@@ -10,18 +10,21 @@ namespace eightball
     {
         static void Main(string[] args)
         {
-            //this reads 
+            //this reads from the Answer.json file 
             var fileData = File.ReadAllText("Answer.json");
+            //Deserialize list for answers in file
             var answers = JsonSerializer.Deserialize<List<Answers>>(fileData);
             while (true)
             {
-                
+                    //Makes random number to pull from list
                     Random rnd = new Random();
+                    //pulls random number and goes to negative 1 so that later on I can add more answers
                     int result = rnd.Next(0, answers.Count-1);
-
+                    //Asks question
                     Console.WriteLine("The not so mighty Eight Ball awaits your question! Ask a question and hit the enter key for the answer! Type quit to stop program.");
-
+                    
                     string inp = Console.ReadLine();
+                    //Exit program if you enter quit
                     if(inp.ToLower()== "quit")
                     {     
                         break;
