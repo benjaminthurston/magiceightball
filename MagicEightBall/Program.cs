db4820d01761984af51b23aa4cb39669c2dfcaed
benjaminthurston <benjamin.thurston87@gmail.com>
2020-07-19 20:44:57 -0400

    adding quit statement

--- MagicEightBall/Program.cs
+++ MagicEightBall/Program.cs
@@ -10,18 +10,31 @@ namespace eightball
     {
         static void Main(string[] args)
         {
+            //this reads 
             var fileData = File.ReadAllText("Answer.json");
             var answers = JsonSerializer.Deserialize<List<Answers>>(fileData);
             while (true)
             {
+                
                     Random rnd = new Random();
                     int result = rnd.Next(0, answers.Count-1);
 
-                    Console.WriteLine("The not so mighty Eight Ball awaits your question! Ask a question and hit the enter key for the answer!");
+                    Console.WriteLine("The not so mighty Eight Ball awaits your question! Ask a question and hit the enter key for the answer! Type quit to stop program.");
 
                     string inp = Console.ReadLine();
+                    if(inp.ToLower()== "quit")
+                    {     
+                        break;
 
-                    Console.WriteLine(answers[result].Answer);
+                    }
+                    else
+                    {
+                        Console.WriteLine(answers[result].Answer);
+
+                    }
+
+                    
+
 
 
             }
